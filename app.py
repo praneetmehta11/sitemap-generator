111c7a48bbcc24b6cf0e61540a1fc86f1b9df4e5
Praneet Mehta <praneet.mehta@signzy.com>
2020-11-02 15:42:09 +0530

    added

--- app.py
+++ app.py
@@ -65,5 +65,5 @@ def stopCrawling():
 
 
 if __name__ == '__main__':
-    app.run(debug=True,host="0.0.0.0",port="5000")
-    # app.run(debug=False)
+    app.run(debug=False,host="0.0.0.0",port="5000")
+    # app.run(debug=True)
